33720cb70c43188d90888ea8174f5f68c5321b01
DESKTOP-RMVJ3EO\Josiane <josianepazini@gmail.com>
2024-08-20 20:14:13 -0300

    put

--- cypress/e2e/pet.cy.js
+++ cypress/e2e/pet.cy.js
@@ -37,11 +37,25 @@ describe('CRUD da entidade Pet', () =>{
       expect(body.status).to.eq(pet.status)
   })
 })
-
-  //Put Pet
+  // Put Pet
+  it('PUT Pet', () => {
+    cy.request({
+      method: 'PUT',
+      url: '/pet',
+      body: petAlterado
+    }).then(({ status, body }) => {
+      expect(status).to.eq(200)
+      expect(body.id).to.eq(petAlterado.id)
+      expect(body.name).to.eq(petAlterado.name)
+      expect(body.category.id).to.eq(petAlterado.category.id)
+      expect(body.category.name).to.eq(petAlterado.category.name)
+      expect(body.tags[0].id).to.eq(petAlterado.tags[0].id)
+      expect(body.tags[0].name).to.eq(petAlterado.tags[0].name)
+      expect(body.status).to.eq(petAlterado.status)
+    })
+  }) // termina PUT
 
   //Delete Pet
-
 
 } )
 
